26919e14ccdc75782cbb926ae797c1ab2efe410a
Sophia Vinci-Booher <svincibo@Sophias-iMac.local>
2020-03-25 12:56:54 -0400

    modified code

--- spade_qa_tractstats.m
+++ spade_qa_tractstats.m
@@ -1,16 +1,14 @@
-% This script reads in FA, OD, and ICVF measures (from Brad Caron's
-% TractProfiles App) for each of the tracts generated (from Dan Bullock's
-% White Matter Segmentation App). It also reads in tract statistics (e.g.,
-% number of streamlines for each tract (from Dan Bullock's Check Tract
-% Quality App). It also reads in behavioral data collected as part of the
-% LWX study.
+% This script reads in SNR values and plots them according to session (pre-
+% vs post-training) and group (expert=3, beginner=2, control=1).
 
 clear all; close all; clc
 format shortG
 
 remove_outliers = 'no';
+% Identify outliers to be removed.
+% outlier = [108 315 318];% 128 315 318];
 
-stats = 'nfibers'; % aka number of streamlines
+stat = 'nfibers';
 
 blprojectid = 'proj-5e61139282b37f2cfe8fdb28';
 
@@ -19,15 +17,6 @@ rootDir = '/Volumes/240/spade/';
 
 % Read in behavioral data.
 beh_data_in_tbl = readtable([rootDir 'supportFiles/SPADE_demographics.csv'], 'TreatAsEmpty', {'.', 'na'});
-
-% Parse table into array and header to make things easier.
-beh_data_in = table2array(beh_data_in_tbl);
-beh_data_in_header = beh_data_in_tbl.Properties.VariableNames;
-
-% Identify outliers to be removed.
-% outlier = [108 128 203 315 316 318];
-
-%% TRACTOGRAPHY.
 
 % Get contents of the directory where the tract measures for this subject are stored.
 grp_contents = dir(fullfile(rootDir, blprojectid));
@@ -39,63 +28,78 @@ grp_contents = grp_contents(arrayfun(@(x) x.name(1), grp_contents) ~= '.');
 grp_contents = grp_contents(arrayfun(@(x) x.name(1), grp_contents) == 's');
 
 % Load in each tract's tractography measures for this subject.
-for i = 1:size(grp_contents, 1)
+sub_count = 0;
+for t = 1:size(grp_contents, 1)
     
-    % Grab subID.
-    sub(i) = str2num(grp_contents(i).name(5:7));
-    
-    % Gather session, for ease.
-    ses(i) = str2num(grp_contents(i).name(end));
-    
-    % Display current session and sub ID.
-    disp(['Session ' s' ', ' grp_contents(i).name])
-    
-    % Get contents of the directory where the tract measures for this subject are stored.
-    sub_contents_tractstats = dir(fullfile(grp_contents(i).folder, grp_contents(i).name, '/dt-neuro-tractmeasures*/*.csv'));
-    
-    % Remove the '.' and '..' files.
-    sub_contents_tractstats = sub_contents_tractstats(arrayfun(@(x) x.name(1), sub_contents_tractstats) ~= '.');
-    
-    % Read in data for this subject and this tract.
-    data_tbl_in = readtable(fullfile(sub_contents_tractstats.folder, sub_contents_tractstats.name));
-    
-    % Convert into header for ease.
-    data_all_in_header = data_tbl_in.TractName;
-    
-    % Get index matrices for hypothesis-driven grouping of WM tracts.
-    for k = 1:length(data_all_in_header)
+    % Only collect values for subjects that have both MRI and behaviora/demographic data.
+    if ~isempty(find((beh_data_in_tbl.No == str2num(grp_contents(t).name(5:7)))))
         
-        % Indices of horizontal tracts.
-        toi_idx(k) = strcmp(data_all_in_header{k}, 'leftSLF1And2') || strcmp(data_all_in_header{k}, 'rightSLF1And2') ...
-            || strcmp(data_all_in_header{k}, 'leftIFOF') || strcmp(data_all_in_header{k}, 'rightIFOF') ...
-            || strcmp(data_all_in_header{k}, 'leftILF') || strcmp(data_all_in_header{k}, 'rightILF') ...
-            || strcmp(data_all_in_header{k}, 'leftArc') || strcmp(data_all_in_header{k}, 'rightArc') ...
-            || strcmp(data_all_in_header{k}, 'leftSLF3') || strcmp(data_all_in_header{k}, 'rightSLF3') ...
-            || strcmp(data_all_in_header{k}, 'leftAslant') || strcmp(data_all_in_header{k}, 'rightAslant') ...
-            || strcmp(data_all_in_header{k}, 'leftTPC') || strcmp(data_all_in_header{k}, 'rightTPC') ...
-            || strcmp(data_all_in_header{k}, 'leftpArc') || strcmp(data_all_in_header{k}, 'rightpArc') ...
-            || strcmp(data_all_in_header{k}, 'leftMDLFspl') || strcmp(data_all_in_header{k}, 'rightMDLFspl') ...
-            || strcmp(data_all_in_header{k}, 'leftVOF') || strcmp(data_all_in_header{k}, 'rightVOF') ...
-            || strcmp(data_all_in_header{k}, 'leftMDLFang') || strcmp(data_all_in_header{k}, 'rightMDLFang');
+        % Display current sub ID.
+        disp(grp_contents(t).name)
         
-    end % end k
-    
-    % Get positions of tracts of interest in data_tbl.
-    toi = find(toi_idx == 1);
-    
-    for t = 1:length(toi)
+        % Update subject counter for when not all subjects are used/needed.
+        sub_count = sub_count + 1;
+            
+            % Get contents of the directory where the tract measures for this subject are stored.
+            sub_contents_tractstats = dir(fullfile(grp_contents(t).folder, grp_contents(t).name, '/dt-neuro-tractmeasures*/*.csv'));
+            
+            % Remove the '.' and '..' files.
+            sub_contents_tractstats = sub_contents_tractstats(arrayfun(@(x) x.name(1), sub_contents_tractstats) ~= '.');
+            
+            % Read in data for this subject and this tract.
+            data_tbl_in = readtable(fullfile(sub_contents_tractstats.folder, sub_contents_tractstats.name));
+            
+            % Convert into header for ease.
+            data_all_in_header = data_tbl_in.TractName;
+            
+            % Get index matrices for hypothesis-driven grouping of WM tracts.
+            for k = 1:length(data_all_in_header)
+                
+                % Indices of horizontal tracts.
+                toi_idx(k) = strcmp(data_all_in_header{k}, 'leftSLF1And2') || strcmp(data_all_in_header{k}, 'rightSLF1And2') ...
+                    || strcmp(data_all_in_header{k}, 'leftIFOF') || strcmp(data_all_in_header{k}, 'rightIFOF') ...
+                    || strcmp(data_all_in_header{k}, 'leftILF') || strcmp(data_all_in_header{k}, 'rightILF') ...
+                    || strcmp(data_all_in_header{k}, 'leftArc') || strcmp(data_all_in_header{k}, 'rightArc') ...
+                    || strcmp(data_all_in_header{k}, 'leftSLF3') || strcmp(data_all_in_header{k}, 'rightSLF3') ...
+                    || strcmp(data_all_in_header{k}, 'leftAslant') || strcmp(data_all_in_header{k}, 'rightAslant') ...
+                    || strcmp(data_all_in_header{k}, 'leftTPC') || strcmp(data_all_in_header{k}, 'rightTPC') ...
+                    || strcmp(data_all_in_header{k}, 'leftpArc') || strcmp(data_all_in_header{k}, 'rightpArc') ...
+                    || strcmp(data_all_in_header{k}, 'leftMDLFspl') || strcmp(data_all_in_header{k}, 'rightMDLFspl') ...
+                    || strcmp(data_all_in_header{k}, 'leftVOF') || strcmp(data_all_in_header{k}, 'rightVOF') ...
+                    || strcmp(data_all_in_header{k}, 'leftMDLFang') || strcmp(data_all_in_header{k}, 'rightMDLFang');
+                
+            end % end k
+            
+            % Get positions of tracts of interest in data_tbl.
+            toi = find(toi_idx == 1);
+            
+            for t2 = 1:length(toi)
+                
+                % Read in mean stat.
+                m(t, t2) = data_tbl_in.StreamlineCount(toi(t2));
+                
+                % Grab tract name for grouping variable.
+                tract{t, t2} = char(data_tbl_in.TractName(toi(t2)));
+                
+            end % end t
+            
+            % Grab subID.
+            sub(sub_count) = str2num(grp_contents(t).name(5:7));
+            
+            % Gather session, for ease.
+            ses(sub_count) = str2num(grp_contents(t).name(end));
+            
+            % Get age group.
+            group(sub_count) = beh_data_in_tbl.DanceLevelCode(find((beh_data_in_tbl.No == str2num(grp_contents(t).name(5:7)))));
+            
+            % Get age in months.
+            age(sub_count) = beh_data_in_tbl.Age(find((beh_data_in_tbl.No == str2num(grp_contents(t).name(5:7)))));
+            
+            clear data_temp toi toi_idx
         
-        % Read in mean stat.
-        m(i, t) = data_tbl_in.StreamlineCount(toi(t));
-        
-        % Grab tract name for grouping variable.
-        tract{i, t} = char(data_tbl_in.TractName(toi(t)));
-        
-    end % end t
+    end % end if exist
     
-    clear data_temp toi toi_idx
-    
-end % end i
+end % end t
 
 % Find empty cells.
 idx = find(cellfun(@isempty,tract));
@@ -110,18 +114,18 @@ m(idx) = NaN;
 list_tract = tract(1, :);
 list_tract = list_tract(~strcmp(list_tract, 'empty'));
 
-% Determine which subIDs appear in both WM and BEH.
-sub_tract_beh = intersect(sub, beh_data_in_tbl.SubjectID);
-
-% Get indices of subjects who appear in both WM and BEH.
+% % Determine which subIDs appear in both WM and BEH.
+sub_tract_beh = intersect(sub, beh_data_in_tbl.No);
+% 
+% % Get indices of subjects who appear in both WM and BEH.
 sub_idx_wm = ismember(sub, sub_tract_beh);
-sub_idx_beh = ismember(beh_data_in_tbl.SubjectID, sub_tract_beh);
+% sub_idx_beh = ismember(beh_data_in_tbl.No, sub_tract_beh);
 
 % Select only subjects who appear in both WM and BEH.
 % Concatenate into one data array and one header array.
 % Remove redundant subID columns.
-data_out = cat(2, sub', ses', beh_data_in_tbl.group_age(sub_idx_beh, :), beh_data_in_tbl.Age_months(sub_idx_beh, :), m(sub_idx_wm, :));
-data_out_header = [{'subID', 'session', 'gp_age', 'cov_age'}, list_tract];
+data_out = cat(2, sub', ses', group', age', m(sub_idx_wm, :));
+data_out_header = [{'subID', 'ses', 'group', 'cov_age'}, list_tract];
 
 % Remove outliers.
 if strcmp(remove_outliers, 'yes') && exist('outlier')
@@ -139,11 +143,72 @@ data_tbl = array2table(data_out, 'VariableNames', data_out_header);
 % Save all variables.
 save([rootDir 'supportFiles/spade_data_' stat '.mat'], 'data_tbl')
 
+% Write out table.
+writetable(data_tbl, fullfile(rootDir, 'supportFiles', ['spade_data_' stat '.csv']));
 
+% Group differences test
+d = m(sub_idx_wm, :);
+for tn = 1:size(d, 2)
 
+    disp(list_tract{tn});
+    
+    disp('Are there streamline count differences between groups at session 1?')
+    tracttotest = d(ses == 1, tn);
+    grouptotest = group(ses == 1)';
+    [p, tableout, stats] = anova1(tracttotest, grouptotest, 'off');
+    disp(['F(' num2str(tableout{2, 3}) ', ' num2str(tableout{3, 3}) ') = ' num2str(tableout{2, 5}) ', p = ' num2str(tableout{2, 6}) '.'])
+    
+    disp('Are there streamline count differences between groups at session 2?')
+    tracttotest = d(ses == 2, tn);
+    grouptotest = group(ses == 2)';
+    [p, tableout, stats] = anova1(tracttotest, grouptotest, 'off');
+    disp(['F(' num2str(tableout{2, 3}) ', ' num2str(tableout{3, 3}) ') = ' num2str(tableout{2, 5}) ', p = ' num2str(tableout{2, 6}) '.'])
+    
+    disp('Are there streamline count differences between sessions in Experts?')
+    tracttotest = d(group == 1, tn);
+    sestotest = ses(group == 1)';
+    [p, tableout, stats] = anova1(tracttotest, sestotest, 'off');
+    disp(['F(' num2str(tableout{2, 3}) ', ' num2str(tableout{3, 3}) ') = ' num2str(tableout{2, 5}) ', p = ' num2str(tableout{2, 6}) '.'])
+    
+    disp('Are there streamline count differences between sessions in Beginners?')
+    tracttotest = d(group == 1, tn);
+    sestotest = ses(group == 1)';
+    [p, tableout, stats] = anova1(tracttotest, sestotest, 'off');
+    disp(['F(' num2str(tableout{2, 3}) ', ' num2str(tableout{3, 3}) ') = ' num2str(tableout{2, 5}) ', p = ' num2str(tableout{2, 6}) '.'])
+    
+    disp('Are there streamline count differences between sessions in Controls?')
+    tracttotest = d(group == 1, tn);
+    sestotest = ses(group == 1)';
+    [p, tableout, stats] = anova1(tracttotest, sestotest, 'off');
+    disp(['F(' num2str(tableout{2, 3}) ', ' num2str(tableout{3, 3}) ') = ' num2str(tableout{2, 5}) ', p = ' num2str(tableout{2, 6}) '.'])
+    
+end % end tn
 
+% Check correlation between session 1 and session 2.
+d = m(sub_idx_wm, :);
+for tn = 1:size(d, 2)
+    
+    [rho(tn), p(tn)] = corr(d(ses==1, tn), d(ses == 2, tn), 'type', 'Pearson', 'rows', 'complete', 'tail', 'both');
+    
+    disp(['Between-session correlation for ' list_tract{tn} ': rho = ' num2str(rho(tn)) ', p = ' num2str(p(tn)) '.']);
+    
+end
 
+% % Check correlation between groups at session 1.
+% for tn = 1:size(d, 2)
+%     
+%     corr([d(ses==1 & group==1, tn), d(ses==1 & group==2, tn), d(ses==1 & group==3, tn)], 'type', 'Pearson', 'rows', 'pairwise', 'tail', 'both')
+%     
+%     disp(['Between-group correlations for ' list_tract{tn} ': rho = ' num2str(rho(2, 1)) ', p = ' num2str(p(2, 1)) '.']);
+%     
+% end
 
+% Look at boxplot of streamline count.
+prepbox = NaN(size(d, 1)/2, size(d, 2));
+prepbox(:, 1:2:size(d, 2)*2) = d(ses==1, :);
+prepbox(:, 2:2:size(d, 2)*2) = d(ses==2, :);
+figure(2)
+boxplot(prepbox, 'colors', 'rb')
 
 
 
