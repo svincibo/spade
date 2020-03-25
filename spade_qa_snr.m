5c5a8c99cce159467af752ee017d74ad2bf47f1c
Sophia Vinci-Booher <svincibo@Sophias-iMac.local>
2020-03-25 10:51:16 -0400

    added plots

--- spade_qa_snr.m
+++ spade_qa_snr.m
@@ -7,10 +7,6 @@ format shortG
 remove_outliers = 'no';
 include = 'all';
 
-c_color = [0 0.4470 0.7410];
-b_color = [0.4660 0.6740 0.1880];
-e_color = [0.6350 0.0780 0.1840];
-
 blprojectid = 'proj-5e61139282b37f2cfe8fdb28';
 
 % Set working directories.
@@ -144,12 +140,10 @@ disp(['F(' num2str(tableout{2, 3}) ', ' num2str(tableout{3, 3}) ') = ' num2str(t
 clear sesttest snrtotest
 
 % Group differences plot
-figure
+figure(1)
 hold on;
 capsize = 0;
 marker = 'o';
-markeredgecolor_h = [0 .73 .73]'; markeredgecolor_v = [.146 0 0]';
-markerfacecolor_h = [0 .73 .73]; markerfacecolor_v = [.146 0 0];
 linewidth = 1.5;
 linestyle = 'none';
 markersize = 10;
@@ -162,6 +156,10 @@ xtickvalues = [1 2 3];
 alphablend = .8;
 ylim_lo = 5;
 ylim_hi = 16;
+
+c_color = [0 0.4470 0.7410];
+b_color = [0.4660 0.6740 0.1880];
+e_color = [0.6350 0.0780 0.1840];
 
 % Controls
 b1 = bar(1, nanmean(snr(group == 1)), 'FaceColor', c_color, 'EdgeColor', c_color, 'FaceAlpha', alphablend);
@@ -206,7 +204,145 @@ a.YLabel.String = 'SNR';
 a.YLabel.FontSize = fontsize;
 pbaspect([1 1 1])
 
-print(fullfile(rootDir, 'plots', ['plot_barplot_snr_' include]), '-dpng')
-print(fullfile(rootDir, 'plots', 'eps', ['plot_barplot_snr_' include]), '-depsc')
+print(fullfile(rootDir, 'plots', ['plot_barplot_snr_bygroup' include]), '-dpng')
+print(fullfile(rootDir, 'plots', 'eps', ['plot_barplot_snr_bygroup' include]), '-depsc')
+
+hold off;
+
+% Session differences plot
+figure(2)
+hold on;
+capsize = 0;
+marker = 'o';
+linewidth = 1.5;
+linestyle = 'none';
+markersize = 10;
+fontname = 'Arial';
+fontsize = 16;
+fontangle = 'italic';
+yticklength = 0;
+xticklength = 0.05;
+xtickvalues = [1 2 3];
+alphablend = .8;
+ylim_lo = 5;
+ylim_hi = 16;
+
+s1_color = [0.41176 0.41176 0.41176];
+s2_color = [0.82745 0.82745 0.82745];
+
+% Session 1
+b1 = bar(1, nanmean(snr(ses == 1)), 'FaceColor', s1_color, 'EdgeColor', s1_color, 'FaceAlpha', alphablend);
+plot([1 1], [nanmean(snr(ses == 1)) - nanstd(snr(ses == 1)) nanmean(snr(ses == 1)) + nanstd(snr(ses == 1))], 'Color', s1_color)
+% Session 2
+b2 = bar(2, nanmean(snr(ses == 2)), 'FaceColor', s2_color, 'EdgeColor', s2_color, 'FaceAlpha', alphablend);
+plot([2 2], [nanmean(snr(ses == 2)) - nanstd(snr(ses == 2)) nanmean(snr(ses == 2)) + nanstd(snr(ses == 2))], 'Color', s2_color)
+
+% xaxis
+xax = get(gca, 'xaxis');
+xax.Limits = [0.5 2.5];
+xax.TickValues = [1 2];
+xax.TickDirection = 'out';
+xax.TickLength = [yticklength yticklength];
+xlabels = {'Pretraining', 'Posttraining'};
+xlabels = cellfun(@(x) strrep(x, ',', '\newline'), xlabels, 'UniformOutput', false);
+xax.TickLabels = xlabels;
+xax.FontName = fontname;
+xax.FontSize = fontsize;
+xax.FontAngle = fontangle;
+
+% yaxis
+yax = get(gca,'yaxis');
+yax.Limits = [ylim_lo ylim_hi];
+yax.TickValues = [ylim_lo (ylim_lo+ylim_hi)/2 ylim_hi];
+yax.TickDirection = 'out';
+yax.TickLength = [xticklength xticklength];
+yax.TickLabels = {num2str(ylim_lo, '%1.0f'), '', num2str(ylim_hi, '%1.0f')};
+yax.FontName = fontname;
+yax.FontSize = fontsize;
+
+% general
+a = gca;
+%     a.TitleFontWeight = 'normal';
+box off
+
+a.YLabel.String = 'SNR';
+
+a.YLabel.FontSize = fontsize;
+pbaspect([1 1 1])
+
+print(fullfile(rootDir, 'plots', ['plot_barplot_snr_bysession_' include]), '-dpng')
+print(fullfile(rootDir, 'plots', 'eps', ['plot_barplot_snr_bysession_' include]), '-depsc')
+
+hold off;
+
+% Session x Group differences plot
+figure(3)
+hold on;
+capsize = 0;
+marker = 'o';
+linewidth = 1.5;
+linestyle = 'none';
+markersize = 10;
+fontname = 'Arial';
+fontsize = 16;
+fontangle = 'italic';
+yticklength = 0;
+xticklength = 0.05;
+xtickvalues = [1 2 3];
+alphablend = .9; alphablend2 = .4;
+ylim_lo = 5;
+ylim_hi = 16;
+
+% Session 1
+b1 = bar(1, nanmean(snr(ses == 1 & group == 1)), 'FaceColor', c_color, 'EdgeColor', c_color, 'FaceAlpha', alphablend, 'EdgeAlpha', alphablend2);
+plot([1 1], [nanmean(snr(ses == 1 & group == 1)) - nanstd(snr(ses == 1 & group == 1)) nanmean(snr(ses == 1 & group == 1)) + nanstd(snr(ses == 1 & group == 1))], 'Color', c_color)
+b2 = bar(2, nanmean(snr(ses == 1 & group == 2)), 'FaceColor', b_color, 'EdgeColor', b_color, 'FaceAlpha', alphablend, 'EdgeAlpha', alphablend2);
+plot([2 2], [nanmean(snr(ses == 1 & group == 2)) - nanstd(snr(ses == 1 & group == 2)) nanmean(snr(ses == 1 & group == 2)) + nanstd(snr(ses == 1 & group == 2))], 'Color', b_color)
+b3 = bar(3, nanmean(snr(ses == 1 & group == 3)), 'FaceColor', e_color, 'EdgeColor', e_color, 'FaceAlpha', alphablend, 'EdgeAlpha', alphablend2);
+plot([3 3], [nanmean(snr(ses == 1 & group == 3)) - nanstd(snr(ses == 1 & group == 3)) nanmean(snr(ses == 1 & group == 3)) + nanstd(snr(ses == 1 & group == 3))], 'Color', e_color)
+% Session 2
+b4 = bar(4, nanmean(snr(ses == 2 & group == 1)), 'FaceColor', c_color, 'EdgeColor', c_color, 'FaceAlpha', alphablend2, 'EdgeAlpha', alphablend2);
+plot([4 4], [nanmean(snr(ses == 2 & group == 1)) - nanstd(snr(ses == 2 & group == 1)) nanmean(snr(ses == 2 & group == 1)) + nanstd(snr(ses == 2 & group == 1))], 'Color', c_color)
+b5 = bar(5, nanmean(snr(ses == 2 & group == 2)), 'FaceColor', b_color, 'EdgeColor', b_color, 'FaceAlpha', alphablend2, 'EdgeAlpha', alphablend2);
+plot([5 5], [nanmean(snr(ses == 2 & group == 2)) - nanstd(snr(ses == 2 & group == 2)) nanmean(snr(ses == 2 & group == 2)) + nanstd(snr(ses == 2 & group == 2))], 'Color', b_color)
+b6 = bar(6, nanmean(snr(ses == 2 & group == 3)), 'FaceColor', e_color, 'EdgeColor', e_color, 'FaceAlpha', alphablend2, 'EdgeAlpha', alphablend2);
+plot([6 6], [nanmean(snr(ses == 2 & group == 3)) - nanstd(snr(ses == 2 & group == 3)) nanmean(snr(ses == 2 & group == 3)) + nanstd(snr(ses == 2 & group == 3))], 'Color', e_color)
+
+% xaxis
+xax = get(gca, 'xaxis');
+xax.Limits = [0.5 6.5];
+xax.TickValues = [1 2 3 4 5 6];
+xax.TickDirection = 'out';
+xax.TickLength = [yticklength yticklength];
+xlabels = {'Pre, Cont.', 'Pre, Beg.', 'Pre, Exp.', ...
+    'Post, Cont.', 'Post, Beg.', 'Post, Exp.',};
+xlabels = cellfun(@(x) strrep(x, ',', '\newline'), xlabels, 'UniformOutput', false);
+xax.TickLabels = xlabels;
+xax.FontName = fontname;
+xax.FontSize = fontsize;
+xax.FontAngle = fontangle;
+
+% yaxis
+yax = get(gca,'yaxis');
+yax.Limits = [ylim_lo ylim_hi];
+yax.TickValues = [ylim_lo (ylim_lo+ylim_hi)/2 ylim_hi];
+yax.TickDirection = 'out';
+yax.TickLength = [xticklength xticklength];
+yax.TickLabels = {num2str(ylim_lo, '%1.0f'), '', num2str(ylim_hi, '%1.0f')};
+yax.FontName = fontname;
+yax.FontSize = fontsize;
+
+% general
+a = gca;
+%     a.TitleFontWeight = 'normal';
+box off
+
+a.YLabel.String = 'SNR';
+
+a.YLabel.FontSize = fontsize;
+pbaspect([1 1 1])
+
+print(fullfile(rootDir, 'plots', ['plot_barplot_snr_bysessionxgroup_' include]), '-dpng')
+print(fullfile(rootDir, 'plots', 'eps', ['plot_barplot_snr_bysessionxgroup_' include]), '-depsc')
 
 hold off;
